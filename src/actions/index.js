5eaaa31562b0113c4be50b5c0b17265d77e631df
Marcos Cannabrava <mpcannabrava@gmail.com>
2020-03-19 19:03:19 -0300

    add redux-form & actions index, show, new

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
-// TODO: add and export your own actions
+const ROOT_URL = "https://wagon-garage-api.herokuapp.com";
+
+export const FETCH_CARS = 'FETCH_CARS';
+
+export function fetchCars(garage) {
+  const promise = fetch(`${ROOT_URL}/${garage}/cars`)
+    .then(response => response.json());
+
+  return {
+    type: FETCH_CARS,
+    payload: promise
+  };
+}
