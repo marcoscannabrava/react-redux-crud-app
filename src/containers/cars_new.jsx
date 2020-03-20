c65154e00730ffc782fe8fc5b0e949a0717fd2c9
Marcos Cannabrava <mpcannabrava@gmail.com>
2020-03-20 12:11:28 -0300

    fix bugs

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -23,19 +23,19 @@ class CarsNew extends Component {
         <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <div className="form-group">
             <label htmlFor="InputBrand">Brand</label>
-            <Field name="brand" type="text" placeholder="Aston Martin" component="input" className="form-control" />
+            <Field name="brand" type="text" placeholder="eg. Aston Martin" component="input" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="InputModel">Model</label>
-            <Field name="model" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+            <Field name="model" type="text" placeholder="eg. DB Mark III" component="input" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="InputOwner">Owner</label>
-            <Field name="owner" type="text" placeholder="James Bond" component="input" className="form-control" />
+            <Field name="owner" type="text" placeholder="eg. James Bond" component="input" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="InputPlate">Plate</label>
-            <Field name="plate" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+            <Field name="plate" type="text" placeholder="AAA####" component="input" className="form-control" />
           </div>
           <button type="submit">Add car</button>
         </form>
--- src/reducers/cars_reducer.jsx
+++ src/reducers/cars_reducer.jsx
@@ -1,9 +1,11 @@
-import { FETCH_CARS } from '../actions';
+import { FETCH_CARS, REMOVE_CAR } from '../actions';
 
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_CARS:
       return action.payload;
+    case REMOVE_CAR:
+      return state.filter((car) => car !== action.payload);
     default:
       return state;
   }
